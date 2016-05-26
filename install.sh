f5275462cb3a731ff22ed4218a028a952b676b44
Rüdiger Block <ruediger.block@plath.de>
2016-05-26 21:06:39 +0200

    added checks some tools and colors

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+function _fail {
+  echo -e "\033[0;31m$*\033[0m"
+}
+
+function _notify {
+  echo -e "\033[0;32m$*\033[0m"
+}
+
+function _warn {
+  echo -e "\033[1;33m$*\033[0m"
+}
+
 if [[ $(whoami) != 'root' ]]; then
   echo "this script is meant to be run as root"
-  echo "try sudo ./install.sh"
+  _fail "try sudo ./install.sh"
   exit 2
 fi
+
+tools='ssh sshpass rdesktop xprop'
+
+for i in $tools; do
+  printf "checking for $i\t"
+  which $i >> /dev/null 2>&1 && _notify "true" || _warn "false"
+done
+
+
 
 script_path=${0%/*}
 
